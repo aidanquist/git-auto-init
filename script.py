a85a39213bfee1417c6d190060fd24ac64c49c66
Aidan <aidanquist@gmail.com>
2022-08-04 18:17:15 -0400

    updated repo_name input to prevent whitespaces

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 #script
-from email import message
 import requests
 from pprint import pprint
 from github import Github
@@ -13,13 +12,15 @@ g = Github(TOKEN)
 authed = g.get_user()
 
 #prompts for the repo name
-repo_name: str = input("repo name: ")
+repo_name: str = input("repo name: ").replace(' ', '-')
 
-try:
-    #Create repo
-    authed.create_repo(name=repo_name, private=True)
-    print("\033[92m {}\033[00m" .format(repo_name + " sucessfully created"))
-except Exception as  e:
-    #prints error message
-    err: str = e.args[1]["message"]
-    print("\033[91m {}\033[00m" .format(err))
+print(repo_name)
+
+# try:
+#     #Create repo
+#     authed.create_repo(name=repo_name, private=True)
+#     print("\033[92m {}\033[00m" .format(repo_name + " sucessfully created"))
+# except Exception as  e:
+#     #prints error message
+#     err: str = e.args[1]["message"]
+#     print("\033[91m {}\033[00m" .format(err))
