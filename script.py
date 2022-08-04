f3aa9f983c90951e7b01647368ae67845c814378
Aidan <aidanquist@gmail.com>
2022-08-04 18:53:44 -0400

    added validation to questions

--- script.py
+++ script.py
@@ -3,22 +3,35 @@ import requests
 from pprint import pprint
 from github import Github
 
+class Invalid_input(Exception):
+    def __init__(self):
+      super().__init__()
+    def __str__(self):
+        return "Invalid input"
 
-
-TOKEN : str = input("Github personal access token: ")
-
-#signs the user in
-g = Github(TOKEN)
-authed = g.get_user()
-
-#prompts for the repo name
-repo_name: str = input("repo name: ").replace(' ', '-')
-
-print(repo_name)
 
 try:
-    #Create repo
-    authed.create_repo(name=repo_name, private=True)
+    #signs the user in
+    TOKEN : str = input("Github personal access token: ")
+    g = Github(TOKEN)
+    authed = g.get_user()
+
+    #prompts for the repo name
+    repo_name: str = input("repo name: ").replace(' ', '-')
+    private_repo_question: chr = input ("private repo (Y/n): ")[0]
+    if (private_repo_question.upper() == "Y"):
+        private_repo: bool = True
+    elif (private_repo_question.upper() == "N"):
+        private_repo: bool = False
+    else:
+        raise Invalid_input()
+except Invalid_input:
+    print("\033[91m {}\033[00m" .format("Invalid Input"))
+    exit()
+
+try:
+    #Create remote repo
+    authed.create_repo(name=repo_name, private=private_repo)
     print("\033[92m {}\033[00m" .format(repo_name + " sucessfully created"))
 except Exception as  e:
     #prints error message
