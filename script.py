6bf63d25340b53cd3fa2261ed3fc3c2fd279e4ec
Aidan Quist <acquist@mtu.edu>
2022-08-10 21:44:05 -0400

    added .env file to store github username and password

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 #script
-import requests
+from dotenv import dotenv_values
 import os
 import sys
 from sys import platform
@@ -16,14 +16,9 @@ repo_name: str = str(sys.argv[0])
 private_repo: bool = True if (sys.argv[0] == "private" or sys.argv[0] == "-p" ) else False
 
 try:
-    if (platform == "linux" or platform == "linux2" or platform == "darwin"):
-        print()
-    else:
-        # Windows...
-        print()
-
     #Create remote repo
     authed.create_repo(name=repo_name, private=private_repo)
+    cmds: list[str] = []
     print("\033[92m {}\033[00m".format(repo_name + " sucessfully created"))
 except Exception as  e:
     #prints error message
